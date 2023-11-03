da8fc70c2e85045d7f4bb64083355beea82802c2
AndresDoig0711 <andres.doig92@gmail.com>
2023-11-03 18:19:43 -0500

    initial commit

---
+++ src/main/java/com/construction/systems/constech/ConstechApplication.java
@@ -0,0 +1,13 @@
+package com.construction.systems.constech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/construction/systems/constech/ConstechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construction.systems.constech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
