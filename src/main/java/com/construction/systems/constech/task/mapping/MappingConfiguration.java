cc99a5d8006cf9e9d153ded2eabb9e442b9ff926
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2023-11-24 10:24:21 -0500

    fix mapping bug

--- src/main/java/com/construction/systems/constech/task/mapping/MappingConfiguration.java
+++ src/main/java/com/construction/systems/constech/task/mapping/MappingConfiguration.java
@@ -3,10 +3,10 @@ package com.construction.systems.constech.task.mapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("learningMappingConfiguration")
+@Configuration("taskMappingConfiguration")
 public class MappingConfiguration {
     @Bean
-    public TaskMapper studentMapper() {
+    public TaskMapper taskMapper() {
         return new TaskMapper();
     }
 }
